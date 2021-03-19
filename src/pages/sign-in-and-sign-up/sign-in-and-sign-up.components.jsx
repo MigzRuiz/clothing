0c0ccee13d46331c53d4e928073f3bc484d582c1
Miguel Ruiz <migz.p.ruiz@gmail.com>
2021-03-18 18:55:40 -0700

    Added the Sign In, Custom Button, Form Input Components on the Signinandsignup Page

---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.components.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import SignIn from "../../components/sign-in/sign-in.components";
+import "./sign-in-and-sign-up.style.scss";
+
+const SignInAndSignUpPage = () => (
+  <div>
+    <SignIn />
+  </div>
+);
+
+export default SignInAndSignUpPage;
